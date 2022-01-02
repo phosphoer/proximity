129c35ff25379b9ce9cba193fa07082ee4a74451
David Evans <david0evans@gmail.com>
2022-01-02 14:25:44 -0800

    try updating activity after lobby join

--- Program.cs
+++ Program.cs
@@ -8,6 +8,48 @@ namespace proximity_mine
   {
     private static readonly long kClientId = 926574841237209158;
 
+    private static void UpdateActivity(Discord.Discord discord, Discord.Lobby lobby)
+    {
+      // Get the special activity secret
+      var activityManager = discord.GetActivityManager();
+      var lobbyManager = discord.GetLobbyManager();
+      var secret = lobbyManager.GetLobbyActivitySecret(lobby.Id);
+
+      // Create a new activity
+      // Set the party id to the lobby id, so everyone in the lobby has the same value
+      // Set the join secret to the special activity secret
+      var activity = new Discord.Activity
+      {
+        Party =
+        {
+          Id = lobby.Id.ToString(),
+          Size =
+          {
+            CurrentSize = lobbyManager.MemberCount(lobby.Id),
+            MaxSize = (int)lobby.Capacity
+          }
+        },
+        Secrets =
+        {
+          Join = secret
+        }
+      };
+
+      // Set this activity as our current one for the user
+      // The activity + party info inside allows people to invite on discord
+      activityManager.UpdateActivity(activity, (result) =>
+      {
+        if (result == Discord.Result.Ok)
+        {
+          Console.WriteLine($"Set activity success, join secret: {activity.Secrets.Join}");
+        }
+        else
+        {
+          Console.WriteLine("Activity Failed");
+        }
+      });
+    }
+
     static void Main(string[] args)
     {
       // Create discord api instance and set up logging
@@ -38,42 +80,7 @@ namespace proximity_mine
 
       lobbyManager.CreateLobby(lobbyTxn, (Discord.Result result, ref Discord.Lobby lobby) =>
       {
-        // Get the special activity secret
-        var secret = lobbyManager.GetLobbyActivitySecret(lobby.Id);
-
-        // Create a new activity
-        // Set the party id to the lobby id, so everyone in the lobby has the same value
-        // Set the join secret to the special activity secret
-        var activity = new Discord.Activity
-        {
-          Party =
-          {
-            Id = lobby.Id.ToString(),
-            Size =
-            {
-              CurrentSize = 1,
-              MaxSize = (int)lobby.Capacity
-            }
-          },
-          Secrets =
-          {
-            Join = secret
-          }
-        };
-
-        // Set this activity as our current one for the user
-        // The activity + party info inside allows people to invite on discord
-        activityManager.UpdateActivity(activity, (result) =>
-        {
-          if (result == Discord.Result.Ok)
-          {
-            Console.WriteLine($"Set activity success, join secret: {activity.Secrets.Join}");
-          }
-          else
-          {
-            Console.WriteLine("Activity Failed");
-          }
-        });
+        UpdateActivity(discord, lobby);
       });
 
       // When we join an activity, try to connect to the relevant lobby
@@ -85,13 +92,14 @@ namespace proximity_mine
         {
           Console.WriteLine("Connected to lobby: {0}", lobby.Id);
 
+          UpdateActivity(discord, lobby);
+
           // Connect to the network of this lobby and send everyone a message
           lobbyManager.ConnectNetwork(lobby.Id);
           lobbyManager.OpenNetworkChannel(lobby.Id, 0, true);
           foreach (var user in lobbyManager.GetMemberUsers(lobby.Id))
           {
-            lobbyManager.SendNetworkMessage(lobby.Id, user.Id, 0,
-                            Encoding.UTF8.GetBytes(String.Format("Hello, {0}!", user.Username)));
+            lobbyManager.SendNetworkMessage(lobby.Id, user.Id, 0, Encoding.UTF8.GetBytes(String.Format("Hello, {0}!", user.Username)));
           }
         });
       };
@@ -104,6 +112,11 @@ namespace proximity_mine
       lobbyManager.OnLobbyMessage += (lobbyID, userID, data) =>
       {
         Console.WriteLine("lobby message: {0} {1}", lobbyID, Encoding.UTF8.GetString(data));
+      };
+
+      lobbyManager.OnNetworkMessage += (lobbyID, userID, channelID, data) =>
+      {
+        Console.WriteLine("channel message: {0} {1} {2}", lobbyID, channelID, Encoding.UTF8.GetString(data));
       };
 
       // Pump the event look to ensure all callbacks continue to get fired.
